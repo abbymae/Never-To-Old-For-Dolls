22a3106ea09a1b7cf4eb63c67654e8ba8242aee1
abbymae <abbypowell@host102.200-3-77.telecom.net.ar>
2014-04-06 14:30:34 -0400

    added a page

---
+++ American_girl_world/Blank_files/Blank.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20140406
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Blank_files/BlankMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
